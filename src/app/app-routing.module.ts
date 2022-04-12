c7738964050aa22c6c918e6dcdab1c36cb90944e
dnyuhm <dylanmeuriot@gmail.com>
2022-04-12 15:12:36 +0200

    Commit 15 : preloading strategy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { PageSignInComponent } from './login/pages/page-sign-in/page-sign-in.component';
 
 const routes: Routes = [
@@ -13,7 +13,9 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
+  ],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
