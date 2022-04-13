1497a4a4fdeca3b2e2b470e7ab4e3a60b65da213
dnyuhm <dylanmeuriot@gmail.com>
2022-04-13 11:59:38 +0200

    Commit 24 : Exercice “version” sur les services & observables

---
+++ src/app/core/services/version.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class VersionService {
+  public numVersion$: BehaviorSubject<number>;
+
+  constructor() {
+    this.numVersion$ = new BehaviorSubject<number>(1);
+
+    console.log(this.numVersion$.value);
+  }
+
+  public incrementVersion() {
+    this.numVersion$.next(this.numVersion$.value + 1);
+  }
+}
